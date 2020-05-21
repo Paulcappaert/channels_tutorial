570bc02231477fe8f52f573ec3189a21859b6136
Paul Cappaert <paul.cappaert@gmail.com>
2020-05-21 19:10:51 -0400

    basic chat room

--- mysite/settings.py
+++ mysite/settings.py
@@ -73,6 +73,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 ASGI_APPLICATION = 'mysite.routing.application'
 
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
+}
+
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
